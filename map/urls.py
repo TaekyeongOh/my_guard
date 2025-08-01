0bb7d119a38a0fcaf0eba7df614080db9e38ede6
TaekyeongOhMe <161999984+TaekyeongOh@users.noreply.github.com>
2025-08-01 15:04:14 +0900

    Feat : 길찾기지도, 지도 구분해서 구현

--- map/urls.py
+++ map/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.kakao_map_view, name='kakao_map_view'),
+    path('search/', views.kakao_map_search_view, name='kakao_map_search_view'),
+
 ]
--- map/views.py
+++ map/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.conf import settings
 
 def index(request):
-    return render(request, 'map/map.html')
+    return render(request, 'map/map_search.html')
 
 # 네이버 지도 api
 # def map_view(request):
@@ -14,5 +14,11 @@ def index(request):
 def kakao_map_view(request):
     kakao_maps_app_key = settings.KAKAO_MAPS_APP_KEY
     return render(request, 'map/map.html', {
+        'kakao_maps_app_key': kakao_maps_app_key
+    })
+
+def kakao_map_search_view(request):
+    kakao_maps_app_key = settings.KAKAO_MAPS_APP_KEY
+    return render(request, 'map/map_search.html', {
         'kakao_maps_app_key': kakao_maps_app_key
     })
